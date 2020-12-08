2f97b0bc16194c3da603a906b4170794e2a2bd0b
William Moody <bill@moody.bmdyy.net>
2020-12-08 16:22:34 +0100

    8 cleaned up

--- 2020/8/sol.py
+++ 2020/8/sol.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
-def run_program(instructions, task):
+from copy import deepcopy
+
+def run_program(instructions):
     acc = 0
     eip = 0
 
-    while True:
+    while eip < len(instructions):
         c_instr = instructions[eip]
-    
-        print 'eip=',eip, 'acc=',acc, 'c_instr=',c_instr
+
+        #print 'eip=',eip, 'acc=',acc, 'c_instr=',c_instr
 
         # check if we were here before
-        if (task == 1 and c_instr[1] > 0) or \
-                (task == 2 and eip > len(instructions)):
-            return acc
+        if (c_instr[1] > 0):
+            return (None,acc)
         c_instr[1] += 1
         
         op = c_instr[0][0]
@@ -27,24 +28,22 @@ def run_program(instructions, task):
             pass
 
         eip += 1
+    return (True,acc) # task 2
 
-instructions = [[line.rstrip().split(" "), 0] for line in open("input", "r")]
+def load_file():
+    return [[line.rstrip().split(" "), 0] for line in open("input", "r")]
 
-print "task_1:",run_program(instructions[:], 1)
+instructions = load_file()
+print "task_1:",run_program(instructions)[1]
 
 # we need to test all nop/jmps to be switched and then see if it exits
 l_nr = 0
 while l_nr < len(instructions):
-    copy = [[line.rstrip().split(" "), 0] for line in open("input", "r")]
-
-    if copy[l_nr][0][0] == "jmp":
-        copy[l_nr][0][0] = "nop"
-        #print l_nr,copy
-        print "task2",run_program(copy, 1)
-
-    elif copy[l_nr][0][0] == "nop":
-        copy[l_nr][0][0] = "jmp"
-        #print l_nr, copy
-        print "task2",run_program(copy, 1)
+    copy = load_file()
+    if copy[l_nr][0][0] == "jmp" or copy[l_nr][0][0] == "nop":
+        copy[l_nr][0][0] = "nop" if copy[l_nr][0][0] == "jmp" else "nop"
+        ret = run_program(copy)
+        if ret[0] == True:
+            print "task_2:",ret[1]
 
     l_nr += 1
